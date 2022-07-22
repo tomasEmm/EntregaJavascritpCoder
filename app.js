7efc4c98a51d05791b009a44a0a63bdb8bf2de23
tomas <emmm.tb37@gmail.com>
2022-07-22 18:44:43 -0300

    tpcoder

---
+++ app.js
@@ -0,0 +1,45 @@
+
+//function suma(num1, num2)
+
+//{var n;
+//n= num1 + num2;
+ //return n;
+
+//}
+
+//var resultado;
+
+//function pedirNombre() {
+//let nombre = prompt ('ingresar nombre')
+
+//alert('Bienvenido' + nombre + 'Disfrute'   )
+
+//}
+//for(let i = 0; i < 3; i++) {
+    //pedirNombre() 
+//}
+
+//function saludar(usuario) {
+   // console.log("Bienvenidos" + usuario)
+
+//}
+//saludar("Remitente")
+
+function datosPersonales (nombre, apellido, edad, peso,altura) {
+    console.log('Nombre'+ nombre)
+    console.log('Apellido' + apellido)
+    console.log('Edad' + edad)
+    console.log('Peso' + peso)
+    console.log('Altura'+ altura)
+}
+
+  
+let NombreUsuario = prompt ('Ingrese su nombre')
+let ApellidoUsuario = prompt ('Ingrese su apeliido')
+let EdadUsuario = prompt('Ingrese su edad')
+let PesoUsuario = prompt('Ingrese su peso')
+let AlturaUsuario = prompt ('Ingrese su peso')
+
+ datosPersonales (NombreUsuario , ApellidoUsuario , EdadUsuario ,
+    PesoUsuario , AlturaUsuario)
+    
